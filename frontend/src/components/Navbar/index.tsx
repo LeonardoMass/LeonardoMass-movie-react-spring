8166bb21d5c4ff2107441598c4b4978d24cebecd
Leonardo Massena <leonardomassena19@gmail.com>
2022-09-02 15:47:53 -0300

    bootstrap-navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+
+function Navbar() {
+    return (
+        <header>
+        <nav className="container">
+            <div className="movie-nav-content">
+                <h1>Movie</h1>
+                <a href="https://github.com/LeonardoMass" target="_blank" rel="noreferrer">
+                    <div className="movie-contact-container">
+                        <GithubIcon />
+                        <p className="movie-contact-link">/LeonardoMass</p>
+                        
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
+    );
+  }
+
+  export default Navbar;
