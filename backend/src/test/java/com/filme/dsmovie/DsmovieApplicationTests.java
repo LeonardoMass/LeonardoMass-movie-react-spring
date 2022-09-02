dec6e1964e680abba9f245caea300384930dacb7
Leonardo Massena <leonardomassena19@gmail.com>
2022-09-02 13:48:47 -0300

    created

---
+++ backend/src/test/java/com/filme/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filme.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
